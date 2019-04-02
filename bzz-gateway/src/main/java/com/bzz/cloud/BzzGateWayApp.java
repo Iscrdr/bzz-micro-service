e54d6c20dbab686cd6f2689c5bfc61f1c73f152a
qianli8811 <624003618@qq.com>
2019-04-02 19:19:50 +0800

    网关中心完善

--- bzz-gateway/src/main/java/com/bzz/cloud/BzzGateWayApp.java
+++ bzz-gateway/src/main/java/com/bzz/cloud/BzzGateWayApp.java
@@ -6,7 +6,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
