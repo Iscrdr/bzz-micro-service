959e0fcc63a68adebdc75d3dace687888e410835
Iscrdr <624003618@qq.com>
2020-05-20 16:54:20 +0800

    第一阶段

---
+++ bzz-module/bzz-rbac-web/src/main/java/com/bzz/cloud/config/JsonResponseAdvice.java
@@ -0,0 +1,43 @@
+package com.bzz.cloud.config;
+
+import com.bzz.cloud.annotation.FieldsExclude;
+import com.bzz.common.Utils.JsonUtils;
+import com.bzz.common.Utils.ResponseData;
+import com.bzz.common.Utils.ResponseResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+//@ControllerAdvice
+public class JsonResponseAdvice implements ResponseBodyAdvice {
+
+    private Logger logger = LoggerFactory.getLogger(JsonResponseAdvice.class);
+    @Override
+    public boolean supports(MethodParameter methodParameter, Class aClass) {
+        return methodParameter.hasMethodAnnotation(FieldsExclude.class);
+    }
+
+    @Override
+    public Object beforeBodyWrite(Object o, MethodParameter methodParameter, MediaType mediaType, Class aClass, ServerHttpRequest serverHttpRequest, ServerHttpResponse serverHttpResponse) {
+        FieldsExclude annotation = methodParameter.getMethod().getAnnotation(FieldsExclude.class);
+        String exclude = annotation.exclude();
+        Class aClass1 = annotation.returnType();
+        JsonUtils.filter(aClass1,"",exclude);
+        logger.info("过滤 {} 中的字段: {} ",aClass.getName(),exclude);
+
+        Map<String,String> msgMap = new HashMap<>();
+        msgMap.put(ResponseData.RESPONE_CODE,"成功");
+
+        String result  = JsonUtils.object2Json(o);
+        Object o1 = JsonUtils.json2Object(result, o.getClass());
+        return o1;
+    }
+}
---
+++ bzz-module/bzz-rbac-web/src/main/java/com/bzz/cloud/config/JsonReturnHandler.java
@@ -0,0 +1,48 @@
+package com.bzz.cloud.config;
+
+import com.bzz.cloud.annotation.FieldsExclude;
+import com.bzz.common.Utils.JsonUtils;
+import com.bzz.common.Utils.ResponseData;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+
+@Component
+public class JsonReturnHandler implements HandlerMethodReturnValueHandler {
+
+    private Logger logger = LoggerFactory.getLogger(JsonResponseAdvice.class);
+
+    @Override
+    public boolean supportsReturnType(MethodParameter methodParameter) {
+        return methodParameter.hasMethodAnnotation(FieldsExclude.class);
+    }
+
+    @Override
+    public void handleReturnValue(Object o, MethodParameter methodParameter, ModelAndViewContainer modelAndViewContainer,
+                                  NativeWebRequest nativeWebRequest) throws Exception {
+        // 设置这个就是最终的处理类了，处理完不再去找下一个类进行处理
+        modelAndViewContainer.setRequestHandled(true);
+
+        FieldsExclude annotation = methodParameter.getMethod().getAnnotation(FieldsExclude.class);
+        String exclude = annotation.exclude();
+        Class aClass1 = annotation.returnType();
+        JsonUtils.filter(aClass1,"",exclude);
+        logger.info("过滤 {} 中的字段: {} ",aClass1.getName(),exclude);
+        String result  = JsonUtils.object2Json(o);
+        HttpServletResponse response = nativeWebRequest.getNativeResponse(HttpServletResponse.class);
+        response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+
+        PrintWriter printWriter = response.getWriter();
+        printWriter.write(result);
+        printWriter.flush();
+
+    }
+}
